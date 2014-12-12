9bd3e662689e9477446756d1e6daaa042402e094
Spencer Florence <florence@ccs.neu.edu>
2014-12-12 15:22:05 -0600

    Added info

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection "markov")
+(define deps '("base" "typed-racket-lib"))
+(define build-deps '("rackunit-lib" "typed-racket-more"))
+(define pkg-desc "A implementation of a markov-chain bot")
+
